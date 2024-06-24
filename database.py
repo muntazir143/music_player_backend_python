9b322f9303af78c7974c85208ff1a92e9a8fcaf1
Muntazir Rizvi <smmr143@gmail.com>
2024-06-24 15:49:07 +0500

    login api with improvements

--- database.py
+++ database.py
@@ -7,4 +7,9 @@ DATABASE_URL = "postgresql://postgres:postgres123@localhost:5432/musicapp"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
 
-db = SessionLocal()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- routes/auth.py
+++ routes/auth.py
@@ -1,15 +1,17 @@
-from fastapi import HTTPException
+from fastapi import Depends, HTTPException
 import uuid
 import bcrypt
+from database import get_db
 from models.user import User
 from pydantic_schemas.user_create import UserCreate
 from fastapi import APIRouter
-from database import db
+from sqlalchemy.orm import Session
+from pydantic_schemas.user_login import UserLogin
 
 router = APIRouter()
 
-@router.post("/signup")
-def signup_user(user: UserCreate):
+@router.post("/signup", status_code=201)
+def signup_user(user: UserCreate, db: Session=Depends(get_db)):
     user_db = db.query(User).filter(User.email == user.email).first()
     if user_db:
         raise HTTPException(400, "User with the same email already exists!")
@@ -22,4 +24,16 @@ def signup_user(user: UserCreate):
     db.commit()
     db.refresh(user_db)
 
+    return user_db
+
+@router.post("/login")
+def login_user(user: UserLogin, db: Session= Depends(get_db)):
+    user_db = db.query(User).filter(User.email == user.email).first()
+    if not user_db:
+        raise HTTPException(400, "User with this email does not exist!")
+    
+    is_match = bcrypt.checkpw(user.password.encode(), user_db.password)
+    if not is_match:
+        raise HTTPException(400, "Incorrect Password!")
+    
     return user_db
