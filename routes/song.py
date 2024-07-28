05c8d0dbb22561d3ba373f6f104b16741066b2b5
Muntazir Rizvi <smmr143@gmail.com>
2024-07-28 17:37:19 +0100

    songs list api

--- routes/song.py
+++ routes/song.py
@@ -45,3 +45,8 @@ def upload_song(song: UploadFile = File(...), thumbnail: UploadFile = File(...),
     db.commit()
     db.refresh(new_song)
     return new_song
+
+@router.get("/list")
+def list_songs(db: Session=Depends(get_db), auth_details=Depends(auth_middleware)):
+    songs = db.query(Song).all()
+    return songs
